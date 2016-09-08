da5d4f3977c58769761d9f8084ffe783a8500198
jaeho lee <spectrick@gmail.com>
2016-09-09 02:29:37 +0900

    initial commit

---
+++ Assets/prefab/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class Enemy : MonoBehaviour {
+	CompositeDisposable disposables = new CompositeDisposable();
+	// Use this for initialization
+	void Start () {
+		var hosts = GameObject.FindGameObjectsWithTag ("host");
+		this.OnMouseDownAsObservable ()
+			.Select (_ => 1)
+			.Scan ((acc, current) => acc + current)
+			.Subscribe (o => {
+			foreach (GameObject host in hosts) {
+					host.GetComponent<Host>().VectorYSubject.OnNext(1);
+			}
+		}).AddTo (disposables);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnDestroy () {
+		disposables.Dispose ();
+	}
+}
---
+++ Assets/script/Host.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class Host : MonoBehaviour {
+	CompositeDisposable disposables = new CompositeDisposable();
+
+	public Subject<int> VectorYSubject;
+
+	public IObservable<int> VectorYAsObservable() {
+		// Lazy Subject
+		return (VectorYSubject ?? (VectorYSubject = new Subject<int>())).AsObservable();
+	}
+
+	// Use this for initialization
+	void Start () {
+		/* single click test */
+		var mouseDownStream = this.OnMouseDownAsObservable ();
+		mouseDownStream
+			.Subscribe (_ => 
+			gameObject.GetComponent<Renderer> ().material.color = Color.cyan
+		).AddTo (disposables);
+
+		/* double click test */
+		mouseDownStream
+			.Buffer(mouseDownStream.Throttle(TimeSpan.FromMilliseconds(250)))
+			.Where(x=>x.Count>1)
+			.Subscribe (_ => 
+			gameObject.GetComponent<Renderer> ().material.color = Color.red
+		).AddTo (disposables);
+
+		Observable.Interval (TimeSpan.FromMilliseconds (1000)).Subscribe (_ =>
+			VectorYSubject.OnNext(-1)
+		).AddTo (disposables);
+
+		VectorYAsObservable().Subscribe (y => transform.Translate(new Vector3(0, y, 0)));
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnDestroy () {
+		disposables.Dispose ();
+	}
+}
